b8922d8f7d17eb714344a0d215b65e9d7a8327fe
Dellarontay <dreadus@stanford.edu>
2017-07-11 09:10:44 -0700

    additional commits for timeline release

--- Alamode/app/models/cartItem.js
+++ Alamode/app/models/cartItem.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 var Schema = mongoose.Schema;
 
-var CartSchema = new Schema({
+var CartItemSchema = new Schema({
     usernameRef : {type:String,required:true,unique:true,lowercase:true},
     items:[]
 });
