85c2c468333ab31566d4d91b7bf93b754a159f76
Dellarontay <dreadus@stanford.edu>
2017-08-06 11:55:02 -0700

    Connection to mongodb ec2 instance

--- Alamode/app.js
+++ Alamode/app.js
@@ -24,20 +24,19 @@ mongoose.Promise = global.Promise;
 /**
  * Connection to mongodb database on port: 27107
  */
-// mongoose.connect('mongodb://localhost:27017/test', function(err){
-//     if(err){
-//         console.log("database not connected " + err);
-//     }else{
-//         console.log("database connected");
-//     }
-// });
+mongoose.connect('mongodb://13.56.151.113:27017/production', function(err){
+    if(err){
+        console.log("database not connected " + err);
+    }else{
+        console.log("database connected");
+    }
+});
 /**
  * Redirect to main template of the single page application
  */
 app.get('*',function(req,res){
     res.sendFile(path.join(__dirname + '/public/views/index.html'));
 });
-
 /**
  * Server starts listening for requests and provide public html files and apis to public users
  */
