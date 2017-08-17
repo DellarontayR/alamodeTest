20e78c0c07141117c9aed0432e090c26516495f8
Dellarontay <dreadus@stanford.edu>
2017-08-16 22:05:23 -0700

    fixed mainshop product list

--- Alamode/public/js/controllers/MainShopController.js
+++ Alamode/public/js/controllers/MainShopController.js
@@ -3,8 +3,30 @@
 alamode.controller('MainShopController', function ($scope, $location, User, Cart, Auth, Product) {
     var mainShop = this;
     mainShop.products = false;
-    $scope.getProducts(function(catalogProducts){
+
+     mainShop.getProductsFromServer = function(callback){
+            (function (){
+                Product.getCatalogProducts().then(function(data){
+                    console.log('data from catalog');
+                    console.log(data);
+                    if(data.data.success){
+                        console.log("get catalog list success");
+                        // home.catalogProducts = data.data.catalogProducts;
+                        return callback(data.data.catalogProducts);
+                    }
+                    else{
+                        console.log("get catalog list failure");
+                    }
+                })
+
+            })();
+    };
+
+    mainShop.getProductsFromServer(function(catalogProducts){
         mainShop.products = catalogProducts;
     });
+    // $scope.getProducts(function(catalogProducts){
+    //     mainShop.products = catalogProducts;
+    // });
 
 });
