0e9ebe988ea81c134f113eaf6907efd48402f623
Dellarontay <dreadus@stanford.edu>
2017-09-18 19:41:39 -0700

    change port to 8080 to test eb

--- Alamode/app.js
+++ Alamode/app.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 var port = process.env.PORT || 8080;
-port = 80;
+port = 8080;
 var morgan = require('morgan');
 var mongoose = require('mongoose');
 
