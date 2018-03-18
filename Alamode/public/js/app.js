1fd7542e2e6ceea6bbff02210b1a698885275694
Dellarontay <dreadus@stanford.edu>
2018-03-17 23:50:38 -0700

    better animations and more control over loggedIn

--- Alamode/public/js/app.js
+++ Alamode/public/js/app.js
@@ -1,6 +1,6 @@
 "use strict";
 var alamode = angular.module('alamode',['appRoutes','userServices','authServices','cartServices',
-'productServices','contactMessageServices','subscriptionServices','stripeServices','gpsServices','ngAnimate','ui.carousel'
+'productServices','contactMessageServices','subscriptionServices','stripeServices','gpsServices','ui.carousel'
 ]).config(function($httpProvider) {
     $httpProvider.interceptors.push('AuthInterceptors');
 });
